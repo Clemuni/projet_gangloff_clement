df1ad73d04ff1c87c512290388aa6b38960a538f
Clement Gangloff <clement.gangloff@yahoo.com>
2022-05-30 16:58:02 +0200

    fix

--- api/index.php
+++ api/index.php
@@ -2,7 +2,9 @@
 
 require '../vendor/autoload.php';
 require_once __DIR__ . '/../bootstrap.php';
-require_once __DIR__ . '/utils.php';
+require_once __DIR__ . './utils.php';
+require_once __DIR__ . './../src/Entity/Client.php';
+require_once __DIR__ . './../src/Entity/Product.php';
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
