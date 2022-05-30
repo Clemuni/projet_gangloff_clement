455b8941f736894f3f70d99e68d13371fa3eff3a
Clement Gangloff <clement.gangloff@yahoo.com>
2022-05-30 15:51:40 +0200

    modifs entities

--- api/index.php
+++ api/index.php
@@ -1,108 +1,122 @@
 <?php
 
-use Firebase\JWT\JWT;
-use Slim\Factory\AppFactory;
-use Tuupola\Middleware\JwtAuthentication;
+require '../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/utils.php';
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Tuupola\Middleware\JwtAuthentication;
+use Slim\Factory\AppFactory;
 
-require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../bootstrap.php';
-const JWT_SECRET = "makey1234567";
- 
 $app = AppFactory::create();
 
-$app->get('/api/hello/{name}', function(Request $request, Response $response, $args): Response
-{
-    $array = [];
-    $array ["nom"] = $args ['name'];
-    $response->getBody()->write(json_encode($array));
+// POST /api/signin
+$app->post('/api/signin', function(Request $request, Response $response, $args){
+    $body = $request->getParsedBody();
+    $email = $body['email'] ?? "";
+    $password = $body['password'] ?? "";
+    $username = $body['username'] ?? "";
+    $first_name = $body['first_name'] ?? "";
+    $last_name = $body['last_name'] ?? "";
+
+    $err = $email == "" || $password == "" || $username == "" || $last_name == "" || $first_name == "";
+    if ($err) {
+        // Problème avec les champs
+        $data["error"] = "Error with the accounts field";
+        $response = $response->withStatus(403);
+        $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        return $response;
+    }
+
+    $clientRespository = Config::getInstance()->entityManager->getRepository('Client');
+    $client = $clientRespository->findOneBy(array("email" => $email));
+    if ($client != null) {
+        // Client déjà existant avec cet email
+        $data["error"] = "Error creating the account";
+        $response = $response->withStatus(403);
+        $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        return $response;
+    }
+
+    $newClient = new Client();
+    $newClient->setEmail($email);
+    $newClient->setFirstName($first_name);
+    $newClient->setLastName($last_name);
+    $newClient->setUsername($username);
+    $newClient->setPassword(password_hash($password, PASSWORD_DEFAULT));
+    Config::getInstance()->entityManager->persist($newClient);
+    Config::getInstance()->entityManager->flush();
+
+    $data["email"] = $email;
+    $response = addHeaders($response);
+    $response = $response->withHeader("Access-Control-Max-Age", JWT_EXPIRATION_TIME);
+    $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+
     return $response;
 });
 
-function createJWT(Response $response): Response
-{
-    $issuedAt = time();
-    $expirationTime = $issuedAt + 600;
-    $payload = array(
-        'id' => 1,
-        'username' => "admin",
-        'iat' => $issuedAt,
-        'exp' => $expirationTime
-    );
+// POST /api/login
+$app->post('/api/login', function(Request $request, Response $response, $args) {
+    $body = $request->getParsedBody();
+    $username = $body['username'] ?? "";
+    $password = $body['password'] ?? "";
 
-    $token_jwt = JWT::encode($payload,JWT_SECRET, "HS256");
-    $response = $response->withHeader("Authorization", "Bearer {$token_jwt}");
+    $err = $username == "" || $password == "";
+    if ($err) {
+        // Problème avec les champs
+        $data["error"] = "Error with the accounts field";
+        $response = $response->withStatus(403);
+        $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        return $response;
+    }
+
+    $clientRespository = Config::getInstance()->entityManager->getRepository('Client');
+    $client = $clientRespository->findOneBy(array("username" => $username));
+    if ($client == null || !password_verify($password, $client->getPassword())) {
+        // Aucun client avec cet email / mdp
+        $data["error"] = "Error with the email or password";
+        $response = $response->withStatus(403);
+        $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        return $response;
+    }
+
+    $data["email"] = $client->getEmail();
+    $data["username"] = $client->getLogin();
+    $data["first_name"] = $client->getFirstName();
+    $data["last_name"] = $client->getLastName();
+    $data["expiration_time"] = time() + JWT_EXPIRATION_TIME;
+    $response = addHeaders($response);
+    $response = createJWT($response, $username);
+    $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
 
     return $response;
-}
+});
 
-$app->get('/api/whoami', function(Request $request, Response $response, $args): Response
-{
-    $data = ["username" => "admin", "lastName" => "Gangloff", "firstName" => "Clément"];
+// GET /api/products
+$app->get('/api/products', function(Request $request, Response $response, $args) {
+    $productRepository = Config::getInstance()->entityManager->getRepository('Product');
+    $products = $productRepository->findAll();
+
+    $data = array();
+    foreach($products as $product) {
+        $productInfo = array(
+            "id" => $product->getId(),
+            "name" => $product->getName(),
+            "cores" => $product->getCores(),
+            "threads" => $product->getThreads(),
+            "brand" => $product->getBrand(),
+            "price" => $product->getPrice()
+        );
+        array_push($data, $productInfo);
+    }
+
+    $response = addHeaders($response);
     $response->getBody()->write(json_encode($data));
     return $response;
 });
 
-$app->post('/api/login', function(Request $request, Response $response, $args): Response
-{
-    $body = $request->getParsedBody();
-    $username = $body["username"] ?? "";
-    $password = $body["password"] ?? "";
-
-    $error = false;
-    if (!preg_match("/[a-zA-Z0-9]{1,20}/", $username)) {
-        $error = true;
-    }
-    if (!preg_match("/[a-zA-Z0-9]{1,20}/", $password)) {
-        $error = true;
-    }
-
-    if (!$error) {
-        // Récupérer l'utilisateur en bd et vérifier le mdp
-        if ($username === "admin" && $password === "mdp") {
-            $response = createJWT($response);
-            $data = ["status" => "success"];
-            $response->getBody()->write(json_encode($data));
-        } else $response = $response->withStatus(401);
-    } else $response = $response->withStatus(401);
-
-    return $response;
-});
-
-$app->get('/api/catalogue/{filtre}', function(Request $request, Response $response, $args) {
-    $filtre = $args['filtre'];
-    $flux = '[{"titre": "linux", "ref": "001", "prix": "20"}, {"titre":"java", "ref:"002", "prix": "21"}]';
-
-    if($filtre){
-        $data = json_decode($flux, true);
-
-        $res = array_filter($data, function($obj) use ($filtre){
-            return strpos($obj["titre"], $filtre) !== false;
-        });
-        $response->getBody()->write(json_encode(array_values($res)));
-    }
-    else{
-        $response->getBody()->write($flux);
-    }
-    return $response;
-});
-
-$options = [
-    "attribute" => "token",
-    "header" => "Authorization",
-    "regexp" => "/Bearer\s+(.*)$/i",
-    "secure" => false,
-    "algorithm" => ["HS256"],
-    "secret" => JWT_SECRET, 
-    "path" => ["/api"], 
-    "ignore" => ["/api/hello", "/api/login", "/api/register"],
-    "error" => function($response, $args){
-        $data = ["ERROR" => 'Connection', 'ERROR' => 'Invalid JWT'];
-        $response = $response->withStatus(401);
-        return $response->withHeader('Content-Type', "application/json")->getBody()->write(json_encode($data));
-    }
-];
-$app->add(new JwtAuthentication($options));
+$app->add(new JwtAuthentication(Config::getInstance()->options));
 
 $app->run();
+
--- bootstrap.php
+++ bootstrap.php
@@ -1,22 +1,58 @@
 <?php
 
+require_once "vendor/autoload.php";
+require_once "./api/utils.php";
+
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
 date_default_timezone_set('Europe/Paris');
 
-require_once "vendor/autoload.php";
+class Config {
+    private static ?Config $instance = null;
+    public ?EntityManager $entityManager = null;
+    public ?mixed $options = null;
 
-$isDevMode = true;
-$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
+    private function __construct()
+    {
+        $isDevMode = true;
+        $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
+        $conn = array(
+            'host' => 'ec2-63-34-153-52.eu-west-1.compute.amazonaws.com',
+            'driver' => 'pdo_pgsql',
+            'user' => 'fcijrquzikextq',
+            'password' => '5aaf1ffdebded57f6c6258bcf84d1148903193ce48e9648ee87bbc78e690a62b',
+            'dbname' => 'd83b92e9vigot1',
+            'port' => '5432'
+        );
+        $this->entityManager = EntityManager::create($conn, $config);
 
-$conn = array(
-    'host' => 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
-    'driver' => 'pdo_pgsql',
-    'user' => 'yefrmfxqeneslg',
-    'password' => '9a39fd544009647a9387a90274e14bbc107e1ba5dcb3d6d15166eea8bea68c0e',
-    'dbname' => 'd3bekr2hsl0254',
-    'port' => '5432'
-);
+        $this->options = [
+            "attribute" => "token",
+            "header" => "Authorization",
+            "regexp" => "/Bearer\s+(.*)$/i",
+            "secure" => false,
+            "algorithm" => ["HS256"],
+            "secret" => JWT_SECRET,
+            "path" => ["/api"],
+            "ignore" => ["/api/login", "/api/signin"],
+            "error" => function ($response, $arguments) {
+                $data["status"] = "error";
+                $data["message"] = $arguments["message"];
+                return $response
+                    ->withHeader("Content-Type", "application/json")
+                    ->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+            }
+        ];
+    }
 
-$entityManager = EntityManager::create($conn, $config);
+    public static function getInstance() : Config {
+        if (self::$instance == null) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+}
+
+
